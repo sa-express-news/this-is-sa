0e2f03fee7137bf3fae8f85074c5f46382fba90e
Kia Farhang <kfarhang0@gmail.com>
2017-09-19 13:39:29 -0500

    Removed Harvey delay note

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,29 +12,28 @@ import largeLogo from '../../images/logo-large.png';
 import './Header.css';
 
 class Header extends Component {
-	
-	props:{
-    	isMobile: boolean
-  	};
 
-  	render(){
-  		const buildLogoMobile = () => (
-  			<FullPhoto src={smallLogo} alt='This is S.A.'/>
-  		);
+	props: {
+		isMobile: boolean
+	};
 
-  		const buildLogoDesktop = () => (
-        <img className='large-logo' src={largeLogo} alt='This is S.A.' />
-  		);
+	render() {
+		const buildLogoMobile = () => (
+			<FullPhoto src={smallLogo} alt='This is S.A.' />
+		);
 
-  		return(
-  			<div className='Header'>
-          {this.props.isMobile && buildLogoMobile()}
-          {!this.props.isMobile && buildLogoDesktop()}
-          <Paragraph text="ExpressNews.com brings you This is S.A., a bimonthly series showcasing the creativity and ingenuity of the city we call home." /> 
-          <p>Note: Production of This is S.A. has been pushed back due to Hurricane Harvey. You'll find the next episode here on Friday, Sept. 15.</p>
-        </div>
-  		)
-  	}
+		const buildLogoDesktop = () => (
+			<img className='large-logo' src={largeLogo} alt='This is S.A.' />
+		);
+
+		return (
+			<div className='Header'>
+				{this.props.isMobile && buildLogoMobile()}
+				{!this.props.isMobile && buildLogoDesktop()}
+				<Paragraph text="ExpressNews.com brings you This is S.A., a bimonthly series showcasing the creativity and ingenuity of the city we call home." />
+			</div>
+		)
+	}
 }
 
 export default Header;
